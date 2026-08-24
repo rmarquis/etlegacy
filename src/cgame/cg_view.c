3b962404f15da77ab4e3be258f9c0cbbb7d20c35
Rémy Marquis <remy.marquis@gmail.com>
2026-08-24 13:41:03 +0200

    cgame: simplify reinforcement countdown calculation
    Replace the float-based truncation with equivalent integer division.
    No behavioral change; removes an unnecessary cast and multiplication.

--- src/cgame/cg_view.c
+++ src/cgame/cg_view.c
@@ -2318,7 +2318,7 @@ static ID_INLINE void CG_PlayAnnouncementTickTock()
 		int warningLeadMsec       = 100;
 		int dwDeployTime          = (ownTeam == TEAM_AXIS) ? cg_redlimbotime.integer : cg_bluelimbotime.integer;
 		int adjustedElapsed       = cgs.aReinfOffset[ownTeam] + (cg.time + warningLeadMsec) - cgs.levelStartTime;
-		int ownReinfTime          = (int)(1 + (dwDeployTime - (adjustedElapsed % dwDeployTime)) * 0.001f);
+		int ownReinfTime          = 1 + (dwDeployTime - (adjustedElapsed % dwDeployTime)) / 1000;
 		int triggerStartReinfTime = dwDeployTime / 1000;
 		int sequenceIndex;
 		qboolean playTock;
